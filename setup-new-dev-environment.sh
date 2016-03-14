07ca8a9a8acd5fa53534518110f265e0f94820f3
Garrett Maring <gmaring@apple.com>
2016-03-14 10:12:04 -0700

    Structuring setup script

---
+++ setup-new-dev-environment.sh
@@ -0,0 +1,33 @@
+## Package Manager
+# Install Homebrew
+
+## Shell
+# Install zsh and make default shell
+
+## XCode
+# Install xcode command line tools. xcode-select --install
+
+## Languages
+# Install Ruby
+# Install Python
+# Install Java
+
+## Ruby
+# Install gems: sass,
+
+## JavaScripts
+# Install node/npm
+# Install important global packages: grunt, grunt-cli, bower, babel, coffeescript
+
+## Databases
+# Install Mongo (permissions)
+# Install postgreSQL
+# Install MySQL
+# Install Cassandra
+
+## One off brew packages
+
+## One off NPM packages
+
+## Set up my system configs
+# Run create-dotfile-links.sh
