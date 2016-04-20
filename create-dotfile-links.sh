8100b29ec4b44facb2b89d704792ae7e5a0032c9
Marcus Buffett <marcusbuffett@me.com>
2016-04-20 09:08:13 -0700

    Now works if it's run like `sh /path/to/script.sh

--- create-dotfile-links.sh
+++ create-dotfile-links.sh
@@ -1,6 +1,6 @@
 echo "Creating links..."
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-for f in $(find . -maxdepth 1 -type f -exec basename {} \;| grep -v "\.git" | grep "\..*") 
+for f in $(find $DIR -maxdepth 1 -type f -exec basename {} \;| grep -v "\.git" | grep "\..*") 
 do
   rm ~/$f
   ln -s $DIR/$f ~/$f
