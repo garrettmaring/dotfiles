94dfc7b3e0306dd5015d321df5ec4f751c9a72c5
Marcus Buffett <marcusbuffett@me.com>
2016-04-20 09:11:08 -0700

    Is there an echo in here?

--- create-dotfile-links.sh
+++ create-dotfile-links.sh
@@ -2,6 +2,7 @@ echo "Creating links..."
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for f in $(find $DIR -maxdepth 1 -type f -exec basename {} \;| grep -v "\.git" | grep "\..*") 
 do
+  echo "Linking ~/$f"
   rm ~/$f
   ln -s $DIR/$f ~/$f
 done
